11d251b1fc4472a8b7787e94a0442f91d5a75e39
wangkodok <wangkodok@gmail.com>
2024-01-04 10:23:29 +0900

    마우스 오버 및 아웃하면 커서 스타일 변경

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 window.addEventListener("DOMContentLoaded", () => {
   const cursor = document.querySelector(".cursor");
   const cursorTail = document.querySelector(".cursor-tail");
+  const hover = document.querySelector(".list-work");
 
   function mouseMove(e) {
     if (e.isTrusted === true) {
       const mouseX = e.pageX;
       const mouseY = e.pageY;
 
-      cursor.style.width = "10px";
-      cursor.style.height = "10px";
+      // cursor.style.width = "10px";
+      // cursor.style.height = "10px";
       cursor.style.opacity = "1";
 
-      cursorTail.style.width = "30px";
-      cursorTail.style.height = "30px";
+      // cursorTail.style.width = "30px";
+      // cursorTail.style.height = "30px";
       cursorTail.style.opacity = "1";
 
       // 마우스 커서 위치를 제어하는 코드
@@ -28,5 +29,20 @@ window.addEventListener("DOMContentLoaded", () => {
         mouseX - document.documentElement.scrollLeft + "px";
     }
   }
+
+  // 마우스 오버하면 마우스 커서 효과
+  function mouseEnter() {
+    cursor.classList.add("cursor-size");
+    cursorTail.classList.remove("cursor-tail");
+  }
+
+  // 마우스 아웃하면 마우스 커서 효과
+  function mouseLeave() {
+    cursor.classList.remove("cursor-size");
+    cursorTail.classList.add("cursor-tail");
+  }
+
   document.addEventListener("mousemove", mouseMove);
+  hover.addEventListener("mouseenter", mouseEnter);
+  hover.addEventListener("mouseleave", mouseLeave);
 });
